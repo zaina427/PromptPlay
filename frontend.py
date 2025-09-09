b37a1ac61e003becb98f65d3775a872fbda16758
zaina427 <zi4435789@gmail.com>
2025-09-09 10:12:27 +0500

    Create frontend.py

---
+++ frontend.py
@@ -0,0 +1,12 @@
+positive_prompt:lion walking in dessert
+negative_prompt:色调艳丽，过曝，静态，细节模糊不清，字幕，风格，作品，画作，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，背景人很多，倒着走
+width:832
+height:480
+seed:82628696717258
+steps:20
+cfg_scale:3
+sampler_name:uni_pc
+scheduler:simple
+frames:24
+fps:16
+output_format:mp4
